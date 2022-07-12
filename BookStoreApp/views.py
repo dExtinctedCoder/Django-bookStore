4a3232374ce6b23c910e4607affdf3c9cf35320c
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-07-12 17:54:25 +0100

    BookStoreApp: Create views for models

--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import BookStoreApp
 
-# Create your views here.
+
+class HomePage(CreateView):
+  model = BookStoreApp
+  template_name = 'home.html'
+  fields = []
+
+
+class CreateBook(CreateView):
+  model = BookStoreApp
+  fields = [
+    'tittle',
+    'author_name',
+    'description',
+    'category',
+    'book_content'
+  ]
+
+  template_name = 'create_books.html';
+  success_url = 'list_books.html';
+
+  def __str__(self):
+    return self.tittle;
+
