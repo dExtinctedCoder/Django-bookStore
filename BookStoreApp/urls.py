b8ae6a21670b3932eaa833a112782b0de6ac1eb7
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-07-12 17:58:36 +0100

    BookStoreApp: create url pattern for HomePage and CreateBook page

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -1,6 +1,9 @@
+from django import views
 from django.urls import path
+from .views import CreateBook, HomePage
 
 
-# urlpatterns = [
-#     path('', .asview)
-# ]
+urlpatterns = [
+    path('', HomePage.as_view()),
+    path('create/', CreateBook.as_view())
+]
