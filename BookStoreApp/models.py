0118bc34f06ed231c84bed185052e4c5ccbc79df
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-07-12 14:22:00 +0100

    BookStoreApp: Create and modify model

--- BookStoreApp/models.py
+++ BookStoreApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookStoreApp(models.Model):
+  tittle = models.CharField(max_length=255);
+  author_name = models.CharField(max_length=100);
+  description = models.BigAutoField(max_length=255);
+  category = models.CharField(max_length=100);
+  pub_date = models.DateField(auto_now_add=True)
+  book_content = models.BigAutoField();
+
