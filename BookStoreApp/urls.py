08b92309ad5756a63292daeef3d2744de1903bcf
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-07-15 21:57:04 +0100

    BookStoreApp: Create urls for project views

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -1,9 +1,13 @@
 from django import views
 from django.urls import path
-from .views import CreateBook, HomePage
+from .views import CreateBook, DeleteBook, DetailBooks, EditBook, HomePage, ListBooks
 
 
 urlpatterns = [
     path('', HomePage.as_view()),
-    path('create/', CreateBook.as_view())
+    path('create/', CreateBook.as_view()),
+    path('list/', ListBooks.as_view()),
+    path('detail/'+ '<pk>', DetailBooks.as_view()),
+    path('edit/'+ '<pk>', EditBook.as_view()),
+    path('delete/'+'<pk>', DeleteBook.as_view())
 ]
