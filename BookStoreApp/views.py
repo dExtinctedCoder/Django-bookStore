fdebfb9ef4017d3bd7f6a9c2095793dbd0d17f0a
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-07-15 21:55:56 +0100

    BookStoreApp: Create views for project  Models

--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -21,8 +21,40 @@ class CreateBook(CreateView):
   ]
 
   template_name = 'create_books.html';
-  success_url = 'list_books.html';
+  success_url = '/list';
 
   def __str__(self):
     return self.tittle;
 
+
+class ListBooks(ListView):
+  model = BookStoreApp
+  fields = [
+    'tittle',
+    'author_name',
+    'category',
+  ]
+  template_name = 'list_books.html'
+
+
+class DetailBooks(DeleteView):
+  model = BookStoreApp
+  template_name = 'detail_books.html'
+
+
+class EditBook(UpdateView):
+  model = BookStoreApp;
+  fields = [
+    'tittle',
+    'description',
+    'category',
+    'book_content'
+  ]
+  template_name = 'edit_books.html';
+  success_url = '/list';
+
+
+class DeleteBook(DeleteView):
+  model = BookStoreApp;
+  template_name = 'delete_books.html';
+  success_url = '/list'
